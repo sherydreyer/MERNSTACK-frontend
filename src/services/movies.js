d00d5821086caa7a8759a38168524ee51d802dc1
sheryldreyer <sheryldreyer2@gmail.com>
2024-06-11 11:46:35 +0200

    first commit

---
+++ src/services/movies.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+class MovieDataService {
+  getAll(page = 0) {
+    return axios.get(`https://mernstack-backend-infa.onrender.com/api/v1/movies?page=${page}`);
+  }
+  get(id) {
+    return axios.get(`https://mernstack-backend-infa.onrender.com/api/v1/movies/id/${id}`);
+  }
+  find(query, by = "title", page = 0) {
+    return axios.get(
+      `https://mernstack-backend-infa.onrender.com/api/v1/movies?${by}=${query}&page=${page}`
+    );
+  }
+  createReview(data) {
+    return axios.post("https://mernstack-backend-infa.onrender.com/api/v1/movies/review", data);
+  }
+  updateReview(data) {
+    return axios.put("https://mernstack-backend-infa.onrender.com/api/v1/movies/review", data);
+  }
+  deleteReview(id, userId) {
+    return axios.delete("https://mernstack-backend-infa.onrender.com/api/v1/movies/review", {
+      data: { review_id: id, user_id: userId },
+    });
+  }
+  getRatings() {
+    return axios.get("https://mernstack-backend-infa.onrender.com/api/v1/movies/ratings");
+  }
+}
+export default new MovieDataService();
